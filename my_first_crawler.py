8a3bb53ee641d92770d7a06e62c20bff9bd292cb
Goelf <happyyang19420@gmail.com>
2017-09-15 21:21:55 +0800

    Add some changes

--- my_first_crawler.py
+++ my_first_crawler.py
@@ -3,26 +3,28 @@
 import requests
 import re
 import json
+import pandas
+import sqlite3
 from bs4 import BeautifulSoup
-from datetime import datetime
+
 
 commentsURL = 'http://comment5.news.sina.com.cn/page/info?version=1&format=js&channel=gn&newsid=comos-{}' \
               '&group=&compress=0&ie=utf-8&oe=utf-8&page=1&page_size=20'
 # Get website url
 
 
-def get_url(url):
+def get_web_url(url):
     res = requests.get(url)
     res.encoding = 'utf-8'
     html_sample = res.text
-    soup = BeautifulSoup(html_sample,'html.parser')
+    soup = BeautifulSoup(html_sample, 'html.parser')
     return soup
 
 # Get news content
 
 
-def get_content(weburl):
-    soup = get_url(weburl)
+def get_contents(web_url):
+    soup = get_web_url(web_url)
     for news in soup.select('.news-item'):
         if len(news.select('h2')) > 0:
             h2 = news.select('h2')[0].text
@@ -34,33 +36,34 @@ def get_content(weburl):
 
 
 def get_title(url):
-    soup = get_url(url)
+    soup = get_web_url(url)
     title = soup.select('#artibodyTitle')[0].text
     return title
 
 
 def get_article(url):
-    soup = get_url(url)
+    soup = get_web_url(url)
     article = ''.join([p.text.strip() for p in soup.select('#artibody p')[:-1]])
     return article
+
+
 # Get news source and time
 
 
 def get_source_time(url):
-    soup = get_url(url)
+    soup = get_web_url(url)
     time_source = soup.select('.time-source')[0].contents[0].strip()
-    # dt = datetime.strptime(time_source.encode('utf-8'), '%Y年%m月%d日%H:%M')
     return time_source
 
 
 def get_source_news(url):
-    soup = get_url(url)
+    soup = get_web_url(url)
     source = soup.select('.time-source span a')[0].text
     return source
 
 
 def get_editor_name(url):
-    soup = get_url(url)
+    soup = get_web_url(url)
     editor = soup.select('.article-editor')[0].text.encode('utf-8').strip('责任编辑：')
     return editor
 
@@ -94,12 +97,39 @@ def get_news_detail(url):
     details['editor'] = editor
     details['total comments'] = comments
 
-    final_result = json.dumps(details,encoding="UTF-8", ensure_ascii=False, sort_keys=False, indent=4)
+    return details
 
-    print final_result
+# collect the news details from each news page
 
 
+def get_page_news(url):
+    getnewslist = []
+    res = requests.get(url)
+    jd = json.loads(res.text.lstrip(' newsloadercallback(').rstrip(');'))
+    for i in jd['result']['data']:
+        getnewslist.append(get_news_detail(i['url']))
+    return getnewslist
 
-get_news_detail('http://news.sina.com.cn/c/nd/2017-09-03/doc-ifykqmrv8359781.shtml')
-get_article('http://news.sina.com.cn/c/nd/2017-09-03/doc-ifykqmrv8359781.shtml')
 
+def get_page_url():
+    url = 'http://api.roll.news.sina.com.cn/zt_list?channel=news&cat_1=gnxw&cat_2==gdxw1||=gatxw||=zs-pl||=mtjj&level==1' \
+          '||=2&show_ext=1&show_all=1&show_num=22&tag=1&format=json&page={}&callback=newsloadercallback&_=1504553539461'
+    news_list = []
+    news_total = []
+    for i in range(1, 3):
+        pagenewsurl = url.format(i)
+        news = get_page_news(pagenewsurl)
+        news_list.extend(news)
+        news_total = json.dumps(news_list, encoding="UTF-8", ensure_ascii=False, sort_keys=False, indent=4)
+    return news_total
+
+
+def get_data_frame():
+    data = get_page_url()
+    df = pandas.DataFrame(eval(data))
+    print df
+
+
+get_data_frame()
+
+get_page_url()
